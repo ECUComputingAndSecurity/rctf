32f59af0e039db7340cd27dc8ce65b173c33cc01
Tom Plant <21111317+pl4nty@users.noreply.github.com>
2022-08-22 21:47:59 +1000

    fix(client): update formatting in footer

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -3,9 +3,9 @@ import withStyles from './jss'
 const Footer = ({ classes }) => (
   <div class={classes.root}>
     <span>
-      Built by <a href='https://tplant.com.au/' target='_blank' rel='noopener noreferrer'>pl4nty</a>
-       with <a href='https://rctf.redpwn.net/' target='_blank' rel='noopener noreferrer'>rCTF</a>
-       and <a href='https://azure.com/' target='_blank' rel='noopener noreferrer'>Azure</a>
+      Built by <a href='https://tplant.com.au/' target='_blank' rel='noopener noreferrer'>pl4nty
+      </a> with <a href='https://rctf.redpwn.net/' target='_blank' rel='noopener noreferrer'>rCTF
+      </a> and <a href='https://azure.com/' target='_blank' rel='noopener noreferrer'>Azure</a>
     </span>
   </div>
 )
