04d128808af98024abaad56efa0fe268081f6270
Tom Plant <21111317+pl4nty@users.noreply.github.com>
2022-08-28 11:33:20 +1000

    fix(client): update footer

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -3,7 +3,7 @@ import withStyles from './jss'
 const Footer = ({ classes }) => (
   <div class={classes.root}>
     <span>
-      Built by <a href='https://tplant.com.au/' target='_blank' rel='noopener noreferrer'>pl4nty
+      Built by <a href='https://tplant.com.au/' target='_blank' rel='noopener noreferrer'>Tom Plant
       </a> with <a href='https://rctf.redpwn.net/' target='_blank' rel='noopener noreferrer'>rCTF
       </a> and <a href='https://azure.com/' target='_blank' rel='noopener noreferrer'>Azure</a>
     </span>
